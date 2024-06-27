59b7464089b289591d45f7d588a9b8ce07d62d98
DEVRAJ SHARMA <70874547+devraj62891@users.noreply.github.com>
2024-06-27 15:24:25 +0530

    datatypes done

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello");// we are using node js so it will give error
+
+console.log(3+3);  console.log("devraj");
+
+console.log(typeof "DEVRAJ");
+console.log(typeof null);
+console.log(typeof undefined);
