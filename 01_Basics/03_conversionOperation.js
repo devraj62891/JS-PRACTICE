cb8d523fa6cd9250305b255d547dbc8e95f579bc
DEVRAJ SHARMA <70874547+devraj62891@users.noreply.github.com>
2024-06-27 16:11:48 +0530

    some operatins done

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -4,9 +4,16 @@ console.log(typeof (score));
 
 
 let valueInNumber = Number(score);
-console.log((valueInNumber));//this is not a pure number
+// console.log((valueInNumber));//this is not a pure number
 
 
 let isLoggedIn =0;
 let booleanIsloggedIn = Boolean(isLoggedIn);
-console.log(booleanIsloggedIn);
+// console.log(booleanIsloggedIn);
+
+
+/////////////////////////////operations/////////////////////
+
+let value=3;
+let negValue= -value;
+console.log(negValue);
