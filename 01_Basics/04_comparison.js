f9333993a92ea29450bf442ea487e3935802e6ab
DEVRAJ SHARMA <70874547+devraj62891@users.noreply.github.com>
2024-06-27 20:08:07 +0530

    comparison done

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,15 @@
+console.log(2>1);  //true or false
+console.log(2>=1);  //true or false
+console.log(2<1);  //true or false
+console.log(2);  //true or false
+console.log(2>1);  //true or false
+
+
+
+
+
+//problems comes here
+console.log("2">1);
+//rembeber that both data should be of same type
+console.log(null>0);
+//remeember that comparison operator converts null to a number treating it as a zero but 
