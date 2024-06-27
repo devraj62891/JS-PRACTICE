092055417ca3903f1a15a9a99fe12c16f278db48
DEVRAJ SHARMA <70874547+devraj62891@users.noreply.github.com>
2024-06-27 15:02:28 +0530

    variables learnign done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553;
+let accountEmail  = "devraj62891@gmail.com";
+var accountPassword = "12345"
+accountCity="KOLKATA";
+let accountState;
+
+// accountId=2; //Not allowed
+
+console.log(accountId);
+
+
+console.table([accountEmail, accountId,accountPassword,accountCity]);
+console.log(accountState);
