8cf962319e8c9817188d4d8ca404caf213635916
DEVRAJ SHARMA <70874547+devraj62891@users.noreply.github.com>
2024-06-28 15:55:19 +0530

    object part 2 done

---
+++ 02_Basics/04_objects.js
@@ -0,0 +1,42 @@
+const tinderUser= new Object();///this is similat to const tinderUser={}; but it is a single ton object and here is multi ton object
+
+
+tinderUser.id="12345"
+tinderUser.name="devraj"
+tinderUser.isLoggedIn=false
+
+// console.log(tinderUser);
+
+const regularUser={
+    email:"some@gmail.com",
+    fullName:{
+        userfullname:{
+            firstname:"devtaj",
+            lastname:"sharma"
+        }
+    }
+}
+
+// console.log(regularUser.fullName.userfullname.lastname);
+
+
+///merging two array
+
+const obj1={1:"a",2:"b"};
+const obj2={3:"c",4:"d"};
+
+// const obj3={obj1,obj2};  ///THIS WONT WORK
+// const obj3= Object.assign(obj1,obj2)   // we will rarely use this insted we will use spread operator
+
+const obj3={...obj1,...obj2}
+console.log(obj3);
+
+
+const users=[
+    {
+        id:1,
+        email:"gmail.com"
+    }
+]
+
+
