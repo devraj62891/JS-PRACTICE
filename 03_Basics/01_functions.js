6662512b6ddbd146247b655ec51d012891854227
DEVRAJ SHARMA <70874547+devraj62891@users.noreply.github.com>
2024-06-28 18:37:55 +0530

    function done

---
+++ 03_Basics/01_functions.js
@@ -0,0 +1,28 @@
+
+
+function syMyname(){
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("H");
+}
+
+// syMyname
+
+
+
+function addTwoNumbers(num1, num2){ //num1 an dnum2  are called parameters
+    return ( num1+num2);
+    // console.log("devraj"); //unrachable
+}
+
+const result=addTwoNumbers(2,9)//here it is called arguments
+
+console.log("result : ",result);
+
+
+
+//Note: when we dont pass any arguments then function returns undefined value
+//== : normal equality operator that converts the value to the same data type 
+//=== strict equal operator which checks strictly if both the value ore of same type
