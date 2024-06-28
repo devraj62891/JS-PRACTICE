d5697b7bbbe0e629bb5093ce33272dc7c8b33a04
DEVRAJ SHARMA <70874547+devraj62891@users.noreply.github.com>
2024-06-28 22:59:37 +0530

    done

---
+++ 03_Basics/02_scopes.js
@@ -0,0 +1,24 @@
+// let a=10;
+// const b=20;
+// var c=20;
+console.log("a");
+console.log("b");
+console.log("c");
+
+//{} == this is called scope
+if(true){
+    let a=10;
+    const b=20;
+    var c=20;
+}
+
+//let == make a data only sccessable in scope
+
+function one(){
+    const username="hitesh"
+
+    function two(){
+        const website=
+        "youtube"
+    }
+}
